4c07dc654dbd187d8c57101ee2d07d964b5522cb
arslanjajja1 <arslanjajja13@gmail.com>
2023-06-18 15:45:51 +0500

    formatting of Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,11 +9,21 @@ const Navbar = () => (
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    <div className="absolute w-[50%] inset-0 gradient-01"/>
+    <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <img src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain"/>
-      <h2 className="font-extrabold text-[24px] leading-[30px] text-white uppercase">Metaversus</h2>
-      <img src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain"/>
+      <img
+        src="/search.svg"
+        alt="search"
+        className="w-[24px] h-[24px] object-contain"
+      />
+      <h2 className="font-extrabold text-[24px] leading-[30px] text-white uppercase">
+        Metaversus
+      </h2>
+      <img
+        src="/menu.svg"
+        alt="menu"
+        className="w-[24px] h-[24px] object-contain"
+      />
     </div>
   </motion.nav>
 );
